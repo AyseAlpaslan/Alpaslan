62eafdc65942ca68075318e089280be67ea1589d
Ayşe Alpaslan <aysealpsln34@gmail.com>
2023-01-19 18:11:12 +0300

    Add project files.

---
+++ Alpaslan/Controllers/ContactController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Alpaslan.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Alpaslan | İletişim ";
+            return View();
+        }
+
+
+
+    }
+}
