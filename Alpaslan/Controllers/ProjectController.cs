19ca88519a58ac59a19cb54d6c783d36da5717ad
Ayşe Alpaslan <aysealpsln34@gmail.com>
2023-01-20 02:38:58 +0300

    Güncelleme yapıldı

---
+++ Alpaslan/Controllers/ProjectController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Alpaslan.Controllers
+{
+    public class ProjectController : Controller
+    {
+        // GET: Project
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Alpaslan | Projeler";
+            return View();
+        }
+    }
+}
