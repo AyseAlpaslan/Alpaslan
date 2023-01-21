a7cb8f77cca8d8abacd6ad153ee1d9861290ddda
Ayşe Alpaslan <aysealpsln34@gmail.com>
2023-01-21 17:27:55 +0300

    Admin Panel Projeye eklendi.

---
+++ Alpaslan/Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using Alpaslan.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Alpaslan.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        AlpaslanDBEntities db = new AlpaslanDBEntities();
+        public ActionResult Index()
+        {
+            var model = db.Authors.ToList();
+            return View();
+        }
+    }
+}
